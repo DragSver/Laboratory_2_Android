95768e5421087f5b2d027cf02f23dcea2b5143cd
Olga Modina <o.modina@finiki.games>
2023-05-21 13:09:14 +0500

    Laboratory 2

--- app/src/main/java/com/example/laboratory2/CardsFragment.kt
+++ app/src/main/java/com/example/laboratory2/CardsFragment.kt
@@ -1,34 +1,37 @@
 package com.example.laboratory2
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.laboratory2.databinding.CardsFragmentBinding
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 class CardsFragment : Fragment() {
+    val cardsLiveData = MutableLiveData<List<Card>>()
+
     var navController: NavController?=null
 
+    lateinit var response: Response<List<CardInfo>>
+//    lateinit var cards: List<Card>
     lateinit var binding: CardsFragmentBinding
     lateinit var adapter: ListAdapter<Card, RecyclerView.ViewHolder>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         adapter = Adapter()
         binding = CardsFragmentBinding.inflate(layoutInflater)
         binding.rcView.layoutManager = LinearLayoutManager(context)
@@ -39,12 +42,17 @@ class CardsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        cardsLiveData.observe(viewLifecycleOwner, Observer { cards -> adapter.submitList(cards) })
         lifecycleScope.launch(Dispatchers.Main) {
-            val response = ApiService.instance().getCardsInfo()
-            val cardsInfo = response.body()!!
-            val cards = cardsInfo.map { cardInfo -> Card.CardFactory.create(cardInfo) }
-            println(cards)
-            adapter.submitList(cards)
+            response = ApiService.instance().getCardsInfo()
+            updateCards()
+//            println(cards)
+//            adapter.submitList(cards)
         }
+    }
+
+    private fun updateCards() {
+        val cardsInfo = response.body()!!
+        cardsLiveData.value = cardsInfo.map { cardInfo -> Card.CardFactory.create(cardInfo) }
     }
 }
